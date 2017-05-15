4604f408b078813b3311242286a99fd2eec60d7f
Richard Allaway <richard.allaway@gmail.com>
2017-05-15 13:06:07 -0400

    adds highly rudimentary image mass centering

--- image.rb
+++ image.rb
@@ -6,17 +6,56 @@ class Image
 
   def initialize(data)
     @source_image = ChunkyPNG::Canvas.from_data_url(data).trim!
+    @source_image.save('source.png')
+
     @image = begin
       dim = [@source_image.width, @source_image.height].max
       canvas = ChunkyPNG::Canvas.new(dim, dim, ChunkyPNG::Color::WHITE).
         replace(@source_image, (dim - @source_image.width) / 2, (dim - @source_image.height) / 2)
       canvas.
-        resample_bilinear!((dim * 1.2).to_i, (dim * 1.2).to_i,).
-        resample_bilinear!((dim * 0.8).to_i, (dim * 0.8).to_i,).
+        # resample_bilinear!((dim * 1.2).to_i, (dim * 1.2).to_i,).
+        # resample_bilinear!((dim * 0.8).to_i, (dim * 0.8).to_i,).
         resample_bilinear!(20,20)
-      ChunkyPNG::Canvas.new(28, 28, ChunkyPNG::Color::WHITE).compose(canvas, 4, 4).tap do |resized_image|
+      ChunkyPNG::Canvas.new(28, 28, ChunkyPNG::Color::WHITE).compose(canvas, 4 + offset(:x), 4 + offset(:y)).tap do |resized_image|
         @mnist_array = build_mnist_array(resized_image.pixels)
       end
+    end
+  end
+
+  def offset(dimension)
+    case dimension
+    when :x
+      left_weight = mean_intensity(quadrant: :left) / mean_intensity
+      right_weight =  mean_intensity(quadrant: :right) / mean_intensity
+      puts "x offset #{weighted_offset(left_weight, right_weight)}"
+      weighted_offset(left_weight, right_weight)
+    when :y
+      top_weight = mean_intensity(quadrant: :top) / mean_intensity
+      bottom_weight =  mean_intensity(quadrant: :bottom) / mean_intensity
+      puts "y offset #{weighted_offset(top_weight, bottom_weight)}"
+      weighted_offset(top_weight, bottom_weight)
+    else
+      raise ArgumentError
+    end
+  end
+
+  def weighted_offset(a, b)
+    absolute_offset = begin
+      case (a - b).abs
+      when 0.18..0.25
+        1
+      when 0.25..0.30
+        2
+      when 0.30..100
+        3
+      else
+        0
+      end
+    end
+    if (a - b).negative?
+      absolute_offset * -1 
+    else
+      absolute_offset
     end
   end
 
@@ -41,9 +80,37 @@ class Image
     end
   end
 
+  def mean_intensity(quadrant: nil)
+    pixels = source_pixels_in_quadrant(quadrant)
+    (pixels.inject(:+) / pixels.length).to_f
+  end
+
+  private :mean_intensity
+
+  def source_pixels_in_quadrant(quadrant)
+    w, h = [@source_image.width, @source_image.height]
+    each_row = @source_image.pixels.each_slice(@source_image.width)
+    case quadrant
+    when :left
+      each_row.map { |row| row.take(w/2) }.flatten
+    when :right
+      each_row.map { |row| row.drop(w/2) }.flatten
+    when :top
+      each_row.take(h/2).flatten
+    when :bottom
+      each_row.drop(h/2).flatten
+    else
+      @source_image.pixels
+    end
+  end
+
+  private :source_pixels_in_quadrant
+
   def destination
     Pathname.new("/app/public/preview_images").tap do |dest|
       dest.mkdir unless dest.exist?
     end
   end
+
+  private :destination
 end
